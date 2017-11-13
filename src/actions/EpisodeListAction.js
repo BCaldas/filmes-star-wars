87aca8e8936841f0e3ac40101418d3eaa2606621
Bruno Caldas <brunopaivacaldas@hotmail.com>
2017-11-13 03:18:26 -0200

    Implementado o Action do componente EpisodeList

---
+++ src/actions/EpisodeListAction.js
@@ -0,0 +1,25 @@
+import SWRequest from "./../SWRequest";
+
+export const ON_EPISODES_CHANGE = "ON_EPISODES_CHANGE";
+
+export const onEpisodesChange = episodes => ({
+    type: ON_EPISODES_CHANGE,
+    payload: {
+        episodes
+    }
+});
+
+export const fetchAll = () => dispatch => {
+    SWRequest
+        .fetchAll()
+        .then(episodes => dispatch(onEpisodesChange(episodes)))
+        .catch(e => alert("Erro ao obter a lista de episódios :/"))
+};
+
+export const handleFavoriteChange = ep => dispatch => {
+    SWRequest
+        .setAsFavorite(ep)
+        .then(() => SWRequest.fetchAll())
+        .then(episodes => dispatch(onEpisodesChange(episodes)))
+        .catch(e => alert("Erro ao mudar"))
+};
